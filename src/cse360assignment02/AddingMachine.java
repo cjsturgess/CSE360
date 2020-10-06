862682dde875b2f85407e79649815a6ba0cd7b50
CJ Sturgess <cj@sturgess.co>
2020-10-05 22:37:54 -0700

    added comments

---
+++ src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,53 @@
+package cse360assignment02;
+
+public class AddingMachine {
+	private int total;
+	 
+	/**
+	 * AddingMachine Constructor
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	 
+	
+	/**
+	 * Get current total from machine
+	 * @return current total
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	  
+	
+	/**
+	 * Add values to the machine.
+	 * @param value how much to add
+	 */
+	public void add (int value) {
+	}
+
+	
+	/**
+	 * Subtracts amount from current machine.
+	 * @param value how much to subtract
+	 */
+	public void subtract (int value) {
+	}
+
+	
+	/**
+	 * String representation of the adding machine.
+	 * @returns string representation
+	 */
+	public String toString () {
+	  return "";
+	}
+	
+
+	/**
+	 * Clears/resets the adding machine.
+	 */
+	public void clear() {
+	}
+}
